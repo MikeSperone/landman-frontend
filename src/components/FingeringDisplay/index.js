301615c57fd2efb2de2e6024447adfd3f60f28cb
speronem <msperone@usnews.com>
2017-09-28 22:32:04 -0400

    initial commit

---
+++ src/components/FingeringDisplay/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './index.css';
+
+class SaxKey extends React.Component {
+    render() {
+        let openClosed = (this.props.keyPressed === "0") ? "open" : "closed";
+        return (
+            <button className={this.props.keyName + " " + openClosed}
+                onClick={() => this.props.onClick()}
+            ></button>
+        );
+    }
+}
+
+export default class FingeringDisplay extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            keyState: props.bin,
+        };
+    }
+
+    handleClick(i) {
+        const keyState = this.state.keyState.slice();
+        const newState = keyState[i] ? "1" : "0";
+        const newKeyState = keyState.substr(0, i) + newState + keyState.substr(i + 1);
+        this.setState({keyState: newKeyState});
+    }
+
+    renderKey(n, v) {
+        return (
+            <SaxKey
+                keyName={n}
+                keyPressed={this.state.keyState[v]}
+                onClick={() => this.handleClick(v)}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div className="fingeringItem">
+                <div className="leftColumn">
+                    <div>
+                        {this.renderKey("octave", 6)}
+                    </div>
+                    <div className="side">
+                        {this.renderKey("highE", 12)}
+                        {this.renderKey("sideC", 13)}
+                        {this.renderKey("sideBFlat", 14)}
+                    </div>
+                    <div className="otherSideKeys">
+                        {this.renderKey("highFSharp", 15)}
+                        {this.renderKey("sideFSharp", 16)}
+                    </div>
+                </div>
+                <div className="centerColumn">
+                    <div className="front">
+                        {this.renderKey("frontF", 7)}
+                    </div>
+                    <div className="fingers">
+                        {this.renderKey("l1", 0)}
+                        {this.renderKey("l2", 1)}
+                        {this.renderKey("l3", 2)}
+                        <hr/>
+                        {this.renderKey("r1", 3)}
+                        {this.renderKey("r2", 4)}
+                        {this.renderKey("r3", 5)}
+                    </div>
+                    <div>
+                        {this.renderKey("eFlat", 21)}
+                        {this.renderKey("lowC", 22)}
+                    </div>
+                </div>
+                <div className="rightColumn">
+                    {this.renderKey("bis", 8)}
+                    <div className="high">
+                        {this.renderKey("highF", 10)}
+                        {this.renderKey("highDSharp", 9)}
+                        {this.renderKey("highD", 11)}
+                    </div>
+                    <div className="sideKeys">
+                        {this.renderKey("gSharp", 17)}
+                        {this.renderKey("lowB", 18)}
+                        {this.renderKey("lowCSharp", 19)}
+                        {this.renderKey("lowBFlat", 20)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FingeringDisplay from './components/FingeringDisplay';
+
+import Slider from 'react-slick';
+
+class SimpleSlider extends React.Component {
+
+    _multiphonic(m) {
+        if (m) return <div>multiphonic</div>;
+    }
+    _other(o) {
+        if (o) return <div>{o.toString()}</div>
+    }
+
+    listItems() {
+
+        return this.props.data.map((d) => {
+            return (
+                <div key={d._id.toString()}>  
+                    <FingeringDisplay bin={d.bin} />
+                <div><b>pitches:</b> {d.pitches.toString()}</div>
+                {this._multiphonic(d.multi)}
+                {this._other(d.other)}
+                </div>
+            );
+        });
+
+    }
+
+    render() {
+
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        
+        return (
+            <Slider { ... settings}>
+                {this.listItems()}
+            </Slider>
+        );
+    }
+}
+
+class List extends React.Component {
+
+    constructor() {
+        super();
+        // getData();
+    }
+
+    componentDidMount() {
+    }
+
+    _getData() {
+        return JSON.parse('[{"_id":"59bc5197d815a836fe32c926","bin":"10000000001110101011101101","pitches":["Bb5","Eb3"],"__v":0},{"_id":"59bc553bd815a836fe32c927","bin":"01000000001000101010000001","pitches":["Ab2","D4"],"__v":0},{"_id":"59bc554bd815a836fe32c928","bin":"11000000001000101010000001","multi":true,"pitches":["Ab2","D4"],"__v":0},{"_id":"59bc5587d815a836fe32c929","bin":"00100000001000100000000001","multi":true,"pitches":["C#2","F#4"],"__v":0},{"_id":"59bc55a2d815a836fe32c92a","bin":"10100000001000100000000001","multi":false,"pitches":["Bb2","G7"],"__v":0}]');
+
+    }
+
+    render() {
+        return (
+            <SimpleSlider data={this._getData()} />
+        );
+    }
+}
+
+ReactDOM.render(
+    <List />,
+    document.getElementById('root')
+);
+
