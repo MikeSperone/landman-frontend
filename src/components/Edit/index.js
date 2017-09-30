b15e924772bac3044e9859937d1c912749897ec3
speronem <msperone@usnews.com>
2017-09-30 11:39:09 -0400

    edit component

---
+++ src/components/Edit/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './index.css';
+
+export default class Edit extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.d = this.props.data;
+    }
+
+    _multiphonic(m) {
+        if (m) return <div>multiphonic</div>;
+    }
+    _other(o) {
+        if (o) return <div>{o.toString()}</div>
+    }
+
+    render() {
+        return (
+            <div>
+                <div class="info">
+                    <div><b>pitches:</b> {this.d.pitches.toString()}</div>
+                    {this._multiphonic(this.d.multi)}
+                    {this._other(this.d.other)}
+                </div>
+                <div className="buttons">
+                    <button className="pure-button pure-button-primary">Edit</button>
+                    <button className="pure-button button-delete">Delete</button>
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FingeringDisplay from './components/FingeringDisplay';
+// import $ from 'jquery';
 
+import FingeringDisplay from './components/FingeringDisplay';
+import Edit from './components/Edit';
 import Slider from 'react-slick';
 
 class SimpleSlider extends React.Component {
-
-    _multiphonic(m) {
-        if (m) return <div>multiphonic</div>;
-    }
-    _other(o) {
-        if (o) return <div>{o.toString()}</div>
-    }
 
     listItems() {
 
@@ -19,9 +14,7 @@ class SimpleSlider extends React.Component {
             return (
                 <div key={d._id.toString()}>  
                     <FingeringDisplay bin={d.bin} />
-                <div><b>pitches:</b> {d.pitches.toString()}</div>
-                {this._multiphonic(d.multi)}
-                {this._other(d.other)}
+                    <Edit data={d} />
                 </div>
             );
         });
@@ -48,15 +41,11 @@ class SimpleSlider extends React.Component {
 
 class List extends React.Component {
 
-    constructor() {
-        super();
-        // getData();
-    }
-
-    componentDidMount() {
-    }
-
     _getData() {
+        // var data;
+        // const url = "http://api.mikesperone.com/landman/v1/alto";
+        // $.getJSON(url, (d) => data = d;);
+        // return data;
         return JSON.parse('[{"_id":"59bc5197d815a836fe32c926","bin":"10000000001110101011101101","pitches":["Bb5","Eb3"],"__v":0},{"_id":"59bc553bd815a836fe32c927","bin":"01000000001000101010000001","pitches":["Ab2","D4"],"__v":0},{"_id":"59bc554bd815a836fe32c928","bin":"11000000001000101010000001","multi":true,"pitches":["Ab2","D4"],"__v":0},{"_id":"59bc5587d815a836fe32c929","bin":"00100000001000100000000001","multi":true,"pitches":["C#2","F#4"],"__v":0},{"_id":"59bc55a2d815a836fe32c92a","bin":"10100000001000100000000001","multi":false,"pitches":["Bb2","G7"],"__v":0}]');
 
     }
