ef4183e79f2a3a1a431814aeb164247fde23a671
speronem <msperone@usnews.com>
2017-10-07 15:06:16 -0400

    get real data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import $ from 'jquery';
+import $ from 'jquery';
 
 import FingeringDisplay from './components/FingeringDisplay';
 import Edit from './components/Edit';
@@ -41,18 +41,21 @@ class SimpleSlider extends React.Component {
 
 class List extends React.Component {
 
-    _getData() {
-        // var data;
-        // const url = "http://api.mikesperone.com/landman/v1/alto";
-        // $.getJSON(url, (d) => data = d;);
-        // return data;
-        return JSON.parse('[{"_id":"59bc5197d815a836fe32c926","bin":"10000000001110101011101101","pitches":["Bb5","Eb3"],"__v":0},{"_id":"59bc553bd815a836fe32c927","bin":"01000000001000101010000001","pitches":["Ab2","D4"],"__v":0},{"_id":"59bc554bd815a836fe32c928","bin":"11000000001000101010000001","multi":true,"pitches":["Ab2","D4"],"__v":0},{"_id":"59bc5587d815a836fe32c929","bin":"00100000001000100000000001","multi":true,"pitches":["C#2","F#4"],"__v":0},{"_id":"59bc55a2d815a836fe32c92a","bin":"10100000001000100000000001","multi":false,"pitches":["Bb2","G7"],"__v":0}]');
-
+    constructor() {
+        super();
+        this.data = [];
+        this.state = {
+            data: this.data
+        };
+    }
+    componentWillMount() {
+        const url = "http://api.mikesperone.com/landman/v1/alto";
+        $.getJSON(url, (d) => this.setState({data:d}));
     }
 
     render() {
         return (
-            <SimpleSlider data={this._getData()} />
+            <SimpleSlider data={this.state.data} />
         );
     }
 }
